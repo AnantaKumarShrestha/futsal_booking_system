b5f4c6e22ea294d9f302b260242f09a0d49821cc
Ananta <anantashrestha45@gmail.com>
2023-12-25 20:59:51 +0545

    added swagger for documentation

--- src/main/java/com/intern/futsalBookingSystem/FutsalBookingSystemApplication.java
+++ src/main/java/com/intern/futsalBookingSystem/FutsalBookingSystemApplication.java
@@ -1,9 +1,12 @@
 package com.intern.futsalBookingSystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Futsal Booking System",description = "API documentation of Futsal Booking System"))
 public class FutsalBookingSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/intern/futsalBookingSystem/restcontroller/UserRestController.java
+++ src/main/java/com/intern/futsalBookingSystem/restcontroller/UserRestController.java
@@ -5,6 +5,7 @@ import com.intern.futsalBookingSystem.dto.SlotDto;
 import com.intern.futsalBookingSystem.dto.SlotsListDto;
 import com.intern.futsalBookingSystem.dto.UserDto;
 import com.intern.futsalBookingSystem.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,42 +22,48 @@ public class UserRestController {
     @Autowired
     private UserService userService;
 
+    @Operation(description = "User SignUp API")
     @PostMapping("/user")
     public UserDto signUpUser(@RequestBody UserDto user){
         return userService.signUpUser(user);
     }
 
+    @Operation(description = "Get Available Futsal List")
     @GetMapping("/users/futsals")
     public List<FutsalListDto> getFutsalList(){
         return userService.getFutsalList();
     }
 
+    @Operation(description = "Get All Game Slots Of Specific Futsal")
     @GetMapping("/users/futsals/{futsalId}/slots")
     public List<SlotsListDto> getAllSlots(@PathVariable("futsalId") UUID futsalId){
         return userService.getAllSlotsOfFutsal(futsalId);
     }
 
+    @Operation(description = "Get All Available Game Slots Of Specific Futsal")
     @GetMapping("/users/futsals/{futsalId}/slots/available")
     public List<SlotsListDto> getAllAvailableSlot(@PathVariable("futsalId") UUID futsalId){
         return userService.getAvailableSLotsOfFutsal(futsalId);
     }
 
+    @Operation(description = "Get Unavailable Futsal Slots Of Specific Futsal")
     @GetMapping("/users/futsals/{futsalId}/slots/unavailable")
     public List<SlotsListDto> getUnavailableSlot(@PathVariable("futsalId") UUID futsalId){
         return userService.getUnavailableSlotsOfFutsal(futsalId);
     }
-
+    @Operation(description = "Book Futsal Game SLots")
     @GetMapping("/users/{userId}/futsal/slots/{slotId}/book")
     public ResponseEntity<SlotDto> bookSlots(@PathVariable("userId") UUID userId, @PathVariable("slotId") UUID slotId){
         return new ResponseEntity<>(userService.bookingFutsalSLots(userId,slotId), HttpStatus.OK);
     }
 
-
+    @Operation(description = "Cancel Booking")
     @GetMapping("/user/futsal/slots/{slotId}/cancel")
     public ResponseEntity<SlotDto> cancelBooking(@PathVariable("slotId") UUID slotId){
         return new ResponseEntity<>(userService.cancelBooking(slotId),HttpStatus.OK);
     }
 
+    @Operation(description = "Get All Own Futsal Game Booking List")
     @GetMapping("/users/{userId}/slots/booked")
     public List<SlotsListDto> getOwnBookings(@PathVariable("userId") UUID userId){
         return userService.getOwnBookings(userId);
